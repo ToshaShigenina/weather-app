9e6f79732f5e1d21389bbab7443ce6fcad392c68
ToshaShigenina <Shigenina@gmail.com>
2022-05-20 18:14:09 +0500

    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import './assets/style/App.css';
+
+function App() {
+  return (
+    <div className="wrapper">
+    <section className="sidebar"></section>
+    <div className="content">
+      <section className="section forecast"></section>
+      <section className="section current"></section>
+    </div>
+  </div>
+  );
+}
+
+export default App;
